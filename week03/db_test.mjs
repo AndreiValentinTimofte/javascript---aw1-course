29847863b8aa4c0d60c833f61ccf3fb64e5e9355
timo <valentin.timofte00@gmail.com>
2025-08-12 23:28:41 +0200

    add week03 files and demonstate async callback behavior

---
+++ week03/db_test.mjs
@@ -0,0 +1,24 @@
+import sqlite from "sqlite3";
+
+const db = new sqlite.Database("questions.sqlite", (err) => {
+if (err)  throw err;
+});
+
+let sql = "SELECT * FROM answer"
+let result = []
+
+// callback: è asincrono. Passa alla successiva operazione. Tutte le operazioni di un callback asincrona che hanno  
+// delle dipendenza al di fuori può rompere l'integrità del codice
+db.all(sql, (err,rows) => {
+    if (err) throw err;
+    for (let row of rows)
+        console.log(row);
+        // result.push(row);
+});
+
+// In questo caso result è vuoto, perché la callback è asincrona
+// for (let r of result) {
+//     console.log(r);
+// }
+
+
